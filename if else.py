7e57c711890a53c2042255641213d9726f117db4
ben <Ctw21.ben.hollandomahony@corketb.ie>
2025-09-11 10:25:23 +0100

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,21 @@
+# starter program week 2 - perhaps this could be a task
+
+def conversation():
+    print("Welcome to my conversation program")
+    print()
+    print("Do you like cycling? Answer yes or no")
+    answer = input()
+    if answer == "yes":
+        print("That's good - you will get very fit")
+    else:
+        print("Perhaps you like some other sport. ")
+    print("Goodbye")
+
+conversation()
+
+# it wont run 
+#it would use the else statment 
+# it is to define the function 
+#to end the convosation 
+# it is part of the syntax 
+# 
