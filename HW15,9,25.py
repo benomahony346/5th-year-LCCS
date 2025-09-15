78ee86a955a7ff7962a3b9f8e0a26d71dec3dd81
ben <Ctw21.ben.hollandomahony@corketb.ie>
2025-09-15 17:29:58 +0100

    Add files via upload

---
+++ HW15,9,25.py
@@ -0,0 +1,79 @@
+def questionone():
+    
+    print(" question one what is the capital of america")
+    print()
+    print("A Dublin")
+    print("B Washington")
+    print("C London")
+    answer = input()
+    if answer == "A":
+        print("wrong answer")
+    elif answer == "B":
+        print("you got it right")
+    else:
+        print("wrong answer")
+questionone()
+print()
+def questiontwo():
+    
+    print(" question two what is the capital of japan")
+    print()
+    print("A tokyo")
+    print("B seol")
+    print("C shinshbuya")
+    answer = input()
+    if answer == "A":
+        print("you got it right")
+    elif answer == "B":
+        print("wrong answer")
+    else:
+        print("wrong answer")
+questiontwo()
+print()
+def questionthree():
+    
+    print(" question three what is the capital of italy ")
+    print()
+    print("A paris")
+    print("B rome")
+    print("C madrid")
+    answer = input()
+    if answer == "A":
+        print("wrong answer")
+    elif answer == "B":
+        print("you got it right")
+    else:
+        print("wrong answer")
+questionthree()
+print()
+def questionfour():
+    
+    print(" question four what is the capital of russia ")
+    print()
+    print("A berlin")
+    print("B moscow")
+    print("C london")
+    answer = input()
+    if answer == "A":
+        print("wrong answer")
+    elif answer == "B":
+        print("you got it right")
+    else:
+        print("wrong answer")
+questionfour()
+print()
+def questionfive():
+    
+    print(" question three what is the capital of luxeMbuorg ")
+    print()
+    print("A luxembourg")
+    print("B amsterdam")
+    print("C vilinius")
+    answer = input()
+    if answer == "A":
+        print("you got it right")
+    elif answer == "B":
+        print("wrong answer")
+    else:
+        print("wrong answer")
+questionfive()
