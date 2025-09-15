72c5554a3cda4104a2cb893f1b1143e0950fe8b3
ben <Ctw21.ben.hollandomahony@corketb.ie>
2025-09-15 14:19:19 +0100

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,9 @@
+def quiz():
+    
+    print(" question one what is the capital of america")
+    print()
+    print("A Dublin")
+    print("B Washington")
+    print("C London")
+    answer = input()
+    
