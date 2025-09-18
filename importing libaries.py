50325fc7db1da126ee4279fc3094f2ae41062510
ben <Ctw21.ben.hollandomahony@corketb.ie>
2025-09-18 10:26:00 +0100

    Add files via upload

---
+++ importing libaries.py
@@ -0,0 +1,29 @@
+# whttps://codeshare.io/eek 4 Loops
+
+from turtle import *
+
+#def square():
+ #   for counter in range(4):
+   #     forward(100)
+  #      right(90)
+
+#square()
+#left(90)
+#square()
+
+
+#def triangle():
+#    for counter in range(3):
+#      forward(150)
+#      right(120)
+#  
+#triangle()
+
+def pentagon():
+    speed(99900000000000)
+    for counter in range(1012):
+        forward(150)
+        left(57)
+        
+pentagon()
+   
