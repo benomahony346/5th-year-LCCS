c6c5fdee4a8fd4527f8e7dc57cedd1e20b24134b
ben <Ctw21.ben.hollandomahony@corketb.ie>
2025-09-22 14:18:48 +0100

    Add files via upload

---
+++ looping3.py
@@ -0,0 +1,62 @@
+from turtle import *
+
+## Task 1: Look at the following code and decide which picture it will generate.
+# Then download it from the shared drive, then save and run it, to check.
+speed(100)
+def demo():
+  pencolor("red")
+  for counter in range (20):
+    forward (150)
+    right(100)
+
+demo()
+
+def dem():
+  pencolor("blue")
+  for counter in range (20):
+    forward (150)
+    right(100)
+
+dem()
+clear()
+def deo():
+  pencolor("orange")
+  for counter in range (200):
+    forward (150)
+    right(95)
+
+deo()
+
+def emo():
+  pencolor("red")
+  for counter in range (20):
+    forward (150)
+    right(100)
+
+emo()
+clear()
+#3.	The following code will draw a petal of a
+#flower. Use the code in a for loop to make a
+#flower as shown on the right.
+
+def petal():
+    circle(50,120)
+    left(60)
+    circle(50,120)
+
+petal()
+clear()
+
+def flower():
+    for counter in range (20):
+        petal()
+clear()
+flower()
+#1.	Modify your square function so that you can input the length of the sides and draw squares of different sizes
+def square(length):
+    for counter in range(4):
+        forward(length)
+        right(90)
+        lenght = int(input("what lenght is your square		"))
+        
+square()
