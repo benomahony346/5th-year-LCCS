1a085a30636ed0115713617da51e04e520f3f5f9
ben <Ctw21.ben.hollandomahony@corketb.ie>
2025-10-21 14:17:26 +0100

    Add files via upload

--- exercise 1.1 to 1.3.py
+++ exercise 1.1 to 1.3.py
@@ -13,3 +13,11 @@ your_year = int(input("what year were you born: ")) # asks you the year you were
 there_age = print(" your age is ", current_year - your_year)# takes away both varibles and prints your age 
 
 #1.3
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter secound number: "))
+sum = n1 + n2
+diffrence = n1-n2
+product = n1*n2
+division = n1/n2 
+floor_division = n1//n2
+remainder = n1%n2 
