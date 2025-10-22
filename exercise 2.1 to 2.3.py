da0596baa83b286f05431cf76e8bfc178d4d7d4d
ben <Ctw21.ben.hollandomahony@corketb.ie>
2025-10-22 14:20:27 +0100

    Add files via upload

---
+++ exercise 2.1 to 2.3.py
@@ -0,0 +1,9 @@
+#exercise 2.1
+
+sentence = input(" what is your sentence: ")
+
+print(sentence.upper())
+print(sentence.lower())
+print(len(sentence))
+print(sentence.find(" "))
+print(sentence.replace(" ", "_"))
