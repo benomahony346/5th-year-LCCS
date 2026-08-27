084ea37958a211bb001e0cd097936ea3ebaf191d
ben <Ctw21.ben.hollandomahony@corketb.ie>
2026-08-27 11:51:30 +0100

    Add files via upload

---
+++ 6th Year Skills Check.py
@@ -0,0 +1,33 @@
+# 6th Year Python Skills
+# Name: Ben holland omahony 
+
+# print("Welcome to the Python Skills Check!")
+# 
+# print("task 1 ")
+# message = "python is fun!"
+# print(message)
+# print ("today is wednesday")
+# 
+# colour = input("please enter your favourite colour : ")
+# movie = input("please enter your favourite movie : ")
+# 
+# print(" your favorite colour is", colour, "your favorite movie is", movie)
+# 
+# age = input ("what is your age :")
+# year = input ("what year were you born : ")
+# 
+# print ("you are", age , "years old and were born in", year)
+# 
+# cost = float(input("how much does the item cost : "))
+# ammount = float(input("how much of the item are you getting : "))
+# total = float(cost * ammount)
+# print("your total is ",total)
+
+num1 = 12
+num2 = 4
+
+print("add=",num1+num2,)
+print("sub=",num1-num2,)
+print("multiply=",num1*num2,)
+print("division=",num1/num2,)
+
