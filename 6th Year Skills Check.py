c2237233010681a7dd57a00e196017e368c1bdfe
ben <Ctw21.ben.hollandomahony@corketb.ie>
2026-08-30 20:40:48 +0100

    Add files via upload

--- 6th Year Skills Check.py
+++ 6th Year Skills Check.py
@@ -21,13 +21,34 @@
 # cost = float(input("how much does the item cost : "))
 # ammount = float(input("how much of the item are you getting : "))
 # total = float(cost * ammount)
-# print("your total is ",total)
+# # print("your total is ",total)
+# 
+# num1 = 12
+# num2 = 4
+# 
+# print("add=",num1+num2,)
+# print("sub=",num1-num2,)
+# print("multiply=",num1*num2,)
+# print("division=",num1/num2,)
 
-num1 = 12
-num2 = 4
 
-print("add=",num1+num2,)
-print("sub=",num1-num2,)
-print("multiply=",num1*num2,)
-print("division=",num1/num2,)
+# Task 6 - If Statements
 
+number = int(input("Please enter a number: "))
+
+if number > 0:
+    print(number, "is a positive number")
+elif number < 0:
+    print(number, "is a negative number")
+else:
+    print(number, "is equal to zero")
+
+
+# Task 7 - Area of a Rectangle
+
+length = float(input("Please enter the length: "))
+width = float(input("Please enter the width: "))
+
+area = length * width
+
+print("The area is:", area)
